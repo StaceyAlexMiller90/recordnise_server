79926497cca00cd6ca10c6532198c3d8467ea1f7
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-04-19 23:30:57 +0200

    DELETE end point finalise to remove record from DB

--- routers/records.js
+++ routers/records.js
@@ -5,8 +5,22 @@ const CollectionItems = require("../models/").collectionItems;
 
 const router = new Router();
 
+router.get('/', authMiddleware, async (req, res, next) => {
+  const userId = parseInt(req.user.id)
+  try {
+    const userRecords = await CollectionItems.findAll({ 
+      where: { userId }, 
+      include: [Record]
+    })
+    const filtered = userRecords.map(record => record.record)
+    res.status(200).send(filtered)
+  } catch(e) {
+    next(e)
+  }
+})
+
 router.post('/', authMiddleware, async (req,res,next) => {
-  const userId = req.user.id
+  const userId = parseInt(req.user.id)
   const {id, title, artist, lowestPrice, genre, style, format, year, imageUrl} = req.body
   try {
     const found = await Record.findOne({
@@ -28,7 +42,21 @@ router.post('/', authMiddleware, async (req,res,next) => {
       })
       await CollectionItems.create({userId, recordId: newRecord.id})
     }
-  res.status(200).send({message: 'Record added'})
+  res.status(200).send({message: 'Record added', newRecord})
+  } catch(e) {
+    next(e)
+  }
+})
+
+router.delete('/', authMiddleware, async (req, res, next) => {
+  const userId = parseInt(req.user.id)
+  const { recordId } = req.body
+  try {
+    const recordToDelete = await CollectionItems.findOne({ 
+      where: { userId, recordId }
+    })
+    await recordToDelete.destroy()
+    res.status(200).send({message: 'record deleted'})
   } catch(e) {
     next(e)
   }
